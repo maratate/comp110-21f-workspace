fd5132a05e9504547a9c2f3b4d1b1f20a4ec2df6
Mara Tate <calamara@email.unc.edu>
2021-09-07 22:41:38 -0400

    Progress on excercise 2

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,17 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730384808"
 
 
 # Begin your solution here...
+letter: str = str(input("What letter do you want to look for?"))
+word: str = str(input("Enter a word:"))
+i: int = 0 
+count: int = 0
+while i < len(word):
+    if (word[i]) == letter:
+        count = count + 1 
+    else:
+        count == 0
+    i = i + 1 
+print("Count:" + str(count))
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,6 +1,15 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "7303804808"
 
 
 # Begin your solution here...
+beat: str = (input("What beat do you want to repeat?"))
+number: int = int(input("How many times do you want to repeat it?"))
+i: int = 0 
+
+while i < 1:
+    print(((beat + " ") * (number - 1) + beat))
+    i = i + 1 
+if number <= 0:
+    print("No beat...")

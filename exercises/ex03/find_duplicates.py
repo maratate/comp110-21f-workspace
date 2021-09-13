b577524435bf2b98f374fa1d31c7fbac8452b3a1
Mara Tate <calamara@email.unc.edu>
2021-09-13 14:35:29 -0400

    Progress on exercise 3

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,18 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730384808"
+word: str = input("Enter a word: ")
+duplicate: bool = False 
+i: int = 0 
+j: int = i + 1
+
+while i <= ((len(word)) - 1):
+    while j <= ((len(word) - 1)):
+        if word[i] == word[j]:
+            duplicate = True 
+        j = j + 1 
+    i = i + 1 
+if duplicate is True: 
+    print("Found duplicate: True")
+else:
+    print("Found duplicate: False")
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,15 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730384808"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+Depth: int = int(input("Depth: "))
+i: int = 0
+j: int = 0
+
+while i < int(Depth):
+    while j < Depth: 
+        print(TREE * (Depth - (Depth - (i + 1))))
+        i = i + 1 
+        j = j + 1  

19c538a2007b1a6f50f2046d34aff59921653725
Mara Tate <calamara@email.unc.edu>
2021-09-13 20:39:02 -0400

    progress on excercise 3

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -3,16 +3,15 @@
 __author__ = "730384808"
 word: str = input("Enter a word: ")
 duplicate: bool = False 
-i: int = 0 
-j: int = i + 1
-
+i: int = 0
 while i <= ((len(word)) - 1):
-    while j <= ((len(word) - 1)):
+    j: int = i + 1 
+    while j <= ((len(word)) - 1):
         if word[i] == word[j]:
             duplicate = True 
         j = j + 1 
     i = i + 1 
-if duplicate is True: 
-    print("Found duplicate: True")
+if duplicate is True:
+    print(" Found duplicate: True")
 else:
     print("Found duplicate: False")

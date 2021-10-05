7a829dabb199a467248a4ceb873c52feaff8809f
Mara Tate <calamara@email.unc.edu>
2021-10-05 16:54:53 -0400

    progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,43 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730384808"
 
 # Define your functions below
+
+
+def only_evens(xs: list[int]) -> list[int]: 
+    """Return even integers from the list."""
+    i: int = 0 
+    list_evens: list[int] = []
+    for x in xs: 
+        if x % 2 == 0:
+            list_evens.append(x)
+        i += 1
+    return (list_evens)
+
+
+def sub(x: list[int], start: int, end: int) -> list[int]:
+    """Return integers bewteen 2 indexes."""
+    list_2: list[int] = []
+    if x == []:
+        return list_2 
+    if start == len(x):
+        return list_2
+    if start < 0:
+        start = 0 
+    if end > len(x):
+        end = (len(x) - 1)
+    while start < end:
+        list_2.append(x[start])
+        start = start + 1 
+    return list_2 
+    
+
+def concat(list_1: list[int], list_2: list[int]) -> list[int]:
+    """Concat the integers of two lists."""
+    list_3: list[int] = []
+    for x in list_1:
+        list_3.append(x)
+    for x in list_2:
+        list_3.append(x)
+    return list_3
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,72 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730384808"
+
+# edge case 
+
+
+def test_only_evens_empty() -> None:
+    x: list[int] = []
+    assert only_evens(x) == []  
+
+# use case 
+
+
+def test_only_evens_only_odds() -> None:
+    x: list[int] = [3, 5, 7, 4]
+    assert only_evens(x) == [4]
+
+# use case 
+
+
+def test_only_evens_find_even_intergers() -> None:
+    x: list[int] = [1, 2, 4, 6]
+    assert only_evens(x) == [2, 4, 6]
+
+# edge case 
+
+
+def test_sub_when_list_is_empty() -> None: 
+    x: list[int] = [] 
+    assert sub(x, 1, 4) == []
+
+# use case 
+
+
+def test_sub_with_indexes() -> None:
+    x: list[int] = [1, 2, 3, 4, 5]
+    assert sub(x, 0, 4) == [1, 2, 3, 4]
+
+# use case 
+
+
+def test_sub_when_start_is_less_than_1() -> None:
+    x: list[int] = [10, 11, 12, 13, 14]
+    assert sub(x, -1, 4) == [10, 11, 12, 13]
+
+# edge case 
+
+
+def test_concat_when_lists_are_empty() -> None:
+    list_1: list[int] = []
+    list_2: list[int] = []
+    assert concat(list_1, list_2) == []
+
+# use case 
+
+
+def test_concat_2_lists() -> None:
+    list_1: list[int] = [1, 2, 3, 4]
+    list_2: list[int] = [5, 6, 7, 8]
+    assert concat(list_1, list_2) == [1, 2, 3, 4, 5, 6, 7, 8]
+
+# use case 
+
+
+def test_concat_list_with_empty_list() -> None:
+    list_1: list[int] = [1, 2, 3, 4]
+    list_2: list[int] = []
+    assert concat(list_1, list_2) == [1, 2, 3, 4]
